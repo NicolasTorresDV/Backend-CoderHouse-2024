d5f48e7f290cf02d699aea4549728762b85072c0
Nicolas Torres <kwonassassin@gmail.com>
2024-01-18 17:32:05 -0300

    Primera entrega

--- classes/Product.js
+++ classes/Product.js
@@ -1,12 +1,14 @@
 class Product{
-    constructor(id,title,description,price,thumbnail,code,stock){
+    constructor(id,title,description,price,status,stock, category,thumbnail,code,){
         this.id = id
         this.title = title
         this.description = description
-        this.price = price
-        this.thumbnail = thumbnail
         this.code = code
+        this.price = price
+        this.status = status || true
         this.stock = stock
+        this.category = category
+        this.thumbnail = thumbnail
     }
 }
 
--- classes/ProductManager.js
+++ classes/ProductManager.js
@@ -1,127 +1,155 @@
 import Product from "./Product.js";
-import fs from "fs"
+import {__dirname} from "../utils.js";
+import { fileURLToPath } from 'url';
+import fs from "fs";
 
-const PATH = "./Products.json"
+const __filename = fileURLToPath(import.meta.url);
 
-class ProductManager{
-    constructor(){
+const PATH = `${__dirname}/Products.json`;
+
+
+
+class ProductManager {
+
+    constructor() {
+
         this.path = PATH
-        this.products = []
 
-        if(!fs.existsSync(this.path.toString())){
-            fs.writeFileSync(this.path.toString(),JSON.stringify(this.products))
-        }
+        this.products = this.loadProducts();
+
     }
 
-    async getProducts(){
 
-        if (await this.checkFileExists(this.path)) {
 
-            const productsJSON = await fs.readFile(this.path, 'utf8');
-    
-            productsArr = JSON.parse(productsJSON);
-    
+    loadProducts() {
+
+        if (fs.existsSync(this.path)) {
+
+            const productsJSON = fs.readFileSync(this.path, 'utf-8');
+
             return JSON.parse(productsJSON);
-          }
 
-        
+        } else {
+
+            fs.writeFileSync(this.path, JSON.stringify([]));
+
+            return [];
+
+        }
+
     }
-    
+
+
+
+    async getProducts() {
+
+        try {
+
+            return this.products;
+
+        } catch (error) {
+
+            console.error("Error al leer el archivo de productos:", error);
+
+            throw error;
+
+        }
+
+    }
+
+
+
     getProductById(id) {
 
-        const productSearched = this.products.find((product) => product.id === id);
-    
-        return productSearched || "Not found";
-    
-      }
+        const productSearched = this.products.find((product) => product.id === id
+        );
 
-      
+        return productSearched || {message: `Product with id ${id} not found`};
 
-    addProduct(title,description,price,thumbnail,code,stock){
+    }
 
-        let product = {title,description,price,thumbnail,code,stock}
+
+
+    addProduct(product) {
+
+        // let product = { title, description, price, thumbnail, code, stock };
+
         if (!this.isValidProduct(product)) {
 
             throw new Error("All fields are required.");
-      
-          }
 
-        this.products = JSON.parse(fs.readFileSync(this.path.toString(),'utf-8'));
-        const existingProduct = this.products.find((existingProduct) => existingProduct.code === code);
+        }
+
+        const existingProduct = this.products.find((existingProduct) => existingProduct.code === product.code);
 
         if (existingProduct) {
-            return "Code is repeated";
+
+            return {message: "Code is repeated"};
+
         }
 
         const newID = this.products.length === 0 ? 1 : this.products[this.products.length - 1].id + 1;
 
-        let newProduct = new Product(newID,title,description,price,thumbnail,code,stock)
-        this.products.push(newProduct)
-        fs.writeFileSync(this.path.toString(), JSON.stringify(this.products));
-        
-        return newProduct      
-    }
+        let newProduct = new Product(newID, product.title, product.description, product.price,product.status || true, product.stock, product.category,product.thumbnail || [])
 
-    updateProduct(product){
-        this.products = JSON.parse(fs.readFileSync(this.path.toString(),'utf-8'));
-        let updatedProducts = this.getProductById(product.id)
-        if (JSON.stringify(updatedProducts) === "{}") {
-            return `Product with id: ${product.id} . Not found`
-        }else{
-            let updatedProducts = this.products.map(
-                    (oneProduct) => {
-                        if (oneProduct.id == product.id){
-                            oneProduct = {...product}
-                        }
-                        return oneProduct
-                    }
-                )
-        
-                fs.writeFileSync(this.path.toString(), JSON.stringify(updatedProducts));
-                return `Product with id: ${product.id} . Has been updated`
-        }
+        this.products.push(newProduct);
+
+        fs.writeFileSync(this.path, JSON.stringify(this.products));
+
+
+        return newProduct;
 
     }
 
-    deleteProduct(id){
-        this.products = JSON.parse(fs.readFileSync(this.path.toString(),'utf-8'));
-        let updatedProducts = this.products.filter(oneProduct => oneProduct.id != id)
-        
-        if (JSON.stringify(updatedProducts) === "{}") {
-            return `Product with id: ${id} . Not found`
-        }else{
-            fs.writeFileSync(this.path.toString(), JSON.stringify(updatedProducts));
-            return `Product with id: ${id} .  Has been deleted`
-        }
+
+
+    updateProduct(product) {
+        console.log(product); 
+
+        let updatedProducts = this.products.map((oneProduct) => {
+
+            if (oneProduct.id == product.id) {
+
+                oneProduct = { ...product };
+
+            }
+
+            return oneProduct;
+
+        });
+
+
+
+        fs.writeFileSync(this.path, JSON.stringify(updatedProducts));
+
+        return {message: `Product with id: ${product.id} . Has been updated`};
 
     }
+
+
+
+    deleteProduct(id) {
+
+        let updatedProducts = this.products.filter(oneProduct => oneProduct.id != id);
+
+        fs.writeFileSync(this.path, JSON.stringify(updatedProducts));
+
+        return {message: `Product with id: ${id} .  Has been deleted`};
+
+    }
+
+
 
     isValidProduct(product) {
 
-        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
-    
+        const requiredFields = ['title', 'description', 'price', 'category', 'code', 'stock'];
+
         return requiredFields.every((field) => product[field]);
-    
-      }
 
-    // Check if file exists
-    async checkFileExists(filePath) {
-
-        try {
-    
-          await fs.access(filePath);
-    
-          return true;
-    
-        } catch (error) {
-    
-          return false;
-    
-        }
-    
-      }
-
+    }
 
 }
 
-export default ProductManager
+
+
+export default ProductManager;
